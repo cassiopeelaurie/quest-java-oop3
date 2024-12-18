d13c10700b6a2656b81799505f239a0c4fe7c808
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-12-18 22:18:20 +0100

    quete 8

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,57 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (!flying && altitude == 0) {
+            flying = true;
+            System.out.println(this.getName() + " takes off in the sky.");
+        } else {
+            System.out.println(this.getName() + " is already flying.");
+        }
+    }
+
+    @Override
+    public void glide() {
+        if (flying && altitude > 0) {
+            System.out.println(this.getName() + " glides into the air.");
+        } else {
+            System.out.println(this.getName() + " can't glide while not flying.");
+        }
+    }
+
+    @Override
+    public void land() {
+        if (flying && altitude < 10) {
+            flying = false;
+            System.out.println(this.getName() + " lands on the ground.");
+        } else if (flying && altitude >= 10) {
+            System.out.println(this.getName() + " is too high, it can't land.");
+        } else {
+            System.out.println(this.getName() + " is already on the ground.");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        altitude += meters;
+        if (flying) {
+            System.out.printf("%s flies upward, altitude: %d%n", this.getName(), altitude);
+        }
+        return altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (flying) {
+            altitude -= meters;
+            if (altitude < 0) {
+                altitude = 0;
+            }
+            System.out.printf("%s flies downward, altitude: %d%n", this.getName(), altitude);
+        }
+        return altitude;
     }
 }
---
+++ Fly.java
@@ -0,0 +1,13 @@
+public interface Fly {
+    void takeOff();
+
+    void glide();
+
+    void land();
+
+    int ascend(int meters);
+
+    int descend(int meters);
+
+}
+
